17e659b2c9bebbe98d9c2449ce2de6db9469ef18
pejkob <112940859+pejkob@users.noreply.github.com>
2024-03-01 20:00:45 +0100

    3. 4. 5 feladat

--- C#/Feladatok/Kalapácsvetés/Pars2012/Pars2012/Program.cs
+++ C#/Feladatok/Kalapácsvetés/Pars2012/Pars2012/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Versenyzo> versenyzok = new List<Versenyzo>();
+        using (StreamReader sr = new StreamReader("Selejtezo2012.txt"))
+        {
+            string elsoSor = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+            }
+        }
 
+        Console.WriteLine($"5. feladat: Versenyzők száma a selejtezőben: {versenyzok.Count} fő");
+        Console.ReadKey();
+    }
+}
---
+++ C#/Feladatok/Kalapácsvetés/Pars2012/Pars2012/Versenyzo.cs
@@ -0,0 +1,45 @@
+﻿public class Versenyzo
+{
+    public string nev { get; set; }
+    public string csoport { get; set; }
+    public string nemzetEsKod { get; set; }
+    public double d1 { get; set; }
+    public double d2 { get; set; }
+    public double d3 { get; set; }
+
+    public Versenyzo(string sor)
+    {
+        
+        string[] sorSplit = sor.Split(';');
+        if (sorSplit.Length != 6)
+        {
+            throw new ArgumentException("Invalid input string format. Expected format: 'name;group;nationality_code;d1;d2;d3'");
+        }
+        this.nev = sorSplit[0];
+        this.csoport = sorSplit[1];
+        this.nemzetEsKod = sorSplit[2];
+        this.d1 = DobasErtek(sorSplit[3]);
+        this.d2 = DobasErtek(sorSplit[4]);
+        this.d3 = DobasErtek(sorSplit[5]);
+    }
+
+    public static double DobasErtek(string dobas)
+    {
+        switch (dobas)
+        {
+            case "X":
+                return -1;
+            case "-":
+                return -2;
+            default:
+                if (double.TryParse(dobas, out double result))
+                {
+                    return result;
+                }
+                else
+                {
+                    throw new FormatException("Invalid input: unable to parse double value.");
+                }
+        }
+    }
+}
