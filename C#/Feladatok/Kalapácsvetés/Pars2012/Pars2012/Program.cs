050e180052323d33061ff9200c3816d05c0c0e14
pejkob <112940859+pejkob@users.noreply.github.com>
2024-03-01 21:21:52 +0100

    10.

--- C#/Feladatok/Kalapácsvetés/Pars2012/Pars2012/Program.cs
+++ C#/Feladatok/Kalapácsvetés/Pars2012/Pars2012/Program.cs
@@ -34,7 +34,7 @@ class Program
         }
         Console.WriteLine($"6. feladat: 78,00 feletti eredménnyel továbbjutott: {tovabbjutott} fő");
         int maxindex = 0;
-        for (int i = 1; i < versenyzok.Count; i++)
+        for (int i = 0; i < versenyzok.Count; i++)
         {
             if (versenyzok[i].Eredmeny() > versenyzok[maxindex].Eredmeny())
             {
@@ -44,6 +44,29 @@ class Program
 
         Console.WriteLine($"9. feladat: A selejtező nyertese\n\tNév: {versenyzok[maxindex].nev}\n\tCsoport: {versenyzok[maxindex].csoport}\n\tNemzet: {Versenyzo.Nemzet(versenyzok[maxindex])}\n\tNemzet kód: {Versenyzo.Kod(versenyzok[maxindex])}\n\tSorozat: {versenyzok[maxindex].d1};{versenyzok[maxindex].d2};{versenyzok[maxindex].d3}\n\tEredmény: {versenyzok[maxindex].Eredmeny()} ");
 
+
+        int helyezes = 0;
+        using (StreamWriter sw = new StreamWriter("Dontos2012.txt"))
+        {
+            sw.WriteLine("Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény");
+
+            while (helyezes < 13 && versenyzok.Count > 0)
+            {
+                helyezes++;
+                int max = 0;
+                for (int i = 0; i < versenyzok.Count; i++)
+                {
+                    if (versenyzok[i].Eredmeny() > versenyzok[max].Eredmeny())
+                    {
+                        max = i;
+                    }
+                }
+                sw.WriteLine($"{helyezes};{versenyzok[max].nev};{versenyzok[max].csoport};{Versenyzo.Nemzet(versenyzok[max])};{Versenyzo.Kod(versenyzok[max])};{versenyzok[max].d1};{versenyzok[max].d2};{versenyzok[max].d3};{versenyzok[max].Eredmeny()}");
+                versenyzok.RemoveAt(max);
+            }
+        }
+
         Console.ReadKey();
     }
 }
+
