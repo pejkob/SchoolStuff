faefa9f3897a3aba60edbdfe1186e9de6828b792
pejkob <112940859+pejkob@users.noreply.github.com>
2024-03-02 11:31:42 +0100

    3. 4. 5. 6.

--- C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Program.cs
+++ C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Program.cs
@@ -19,6 +19,74 @@
             osszesMulasztott += item.mulasztottOrakSzama;
         }
         Console.WriteLine($"2. feladat\n\tÖsszes mulasztott órák száma: {osszesMulasztott} óra.");
+        int bekertSzam;
+        do
+        {
+            Console.Write("3. fealdat\n\tKérem adjon meg egy napot: ");
+        } while (!int.TryParse(Console.ReadLine(), out bekertSzam) || bekertSzam < 1 || bekertSzam > 30);
+
+        Console.Write("\tTanuló neve: ");
+        string bekertNev = Console.ReadLine();
+
+        bool hianyzott = false;
+
+        foreach (var item in hianyzasokLista)
+        {
+            if (item.nev==bekertNev)
+            {
+                hianyzott = true;
+                break;
+            }
+        }
+        Console.WriteLine("4. feladat");
+        Console.Write(hianyzott?"\tA tanuló hiányzott szeptemberben":"\tA tanuló nem hiányzott szeptemberben" );
+
+        List<Hianyzasok> hianyzasokAdottNapon = new List<Hianyzasok>();
+        foreach (var item in hianyzasokLista)
+        {
+            if (item.elsoNap==bekertSzam||item.utolsoNap==bekertSzam)
+            {
+                hianyzasokAdottNapon.Add(item);
+            }
+        }
+
+        Console.WriteLine($"5. feladat: Hiányzók 2017.09.{bekertSzam}-n:");
+        if (hianyzasokAdottNapon.Count==0)
+        {
+            Console.WriteLine("\tNem volt hiányzó");
+        }
+        else
+        {
+            foreach (var item in hianyzasokAdottNapon)
+            {
+                Console.WriteLine($"\t{item.nev} {item.osztaly}");
+            }
+        }
+
+        Dictionary<string, int> osszesites = new Dictionary<string, int>();
+
+        foreach (var item in hianyzasokLista)
+        {
+            if (!osszesites.ContainsKey(item.osztaly))
+            {
+                osszesites[item.osztaly] = item.mulasztottOrakSzama;
+            }
+            else
+            {
+                osszesites[item.osztaly] += item.mulasztottOrakSzama;
+            }
+        }
+
+        using (StreamWriter sw = new StreamWriter("osszesites.csv"))
+        {
+            foreach (var item in osszesites)
+            {
+                sw.WriteLine($"{item.Key};{item.Value}");
+            }
+        }
+
+
+
         Console.ReadKey();
     }
 }
