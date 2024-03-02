3ac5f3936311be7ad4d0cfee077a989955dda872
pejkob <112940859+pejkob@users.noreply.github.com>
2024-03-02 10:46:45 +0100

    1. 2.

--- C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Hianyzasok.cs
+++ C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Hianyzasok.cs
@@ -1,11 +1,21 @@
 ﻿using System;
-namespace Hianyzasok
-{
+
 	public class Hianyzasok
 	{
-		public Hianyzasok()
+		public Hianyzasok(string nev, string osztaly, int elso, int utolso, int mulasztott)
 		{
+			this.nev = nev;
+			this.osztaly = osztaly;
+			this.elsoNap = elso;
+			this.utolsoNap = utolso;
+			this.mulasztottOrakSzama = mulasztott;
 		}
+
+		public string nev { get; set; }
+		public string osztaly { get; set; }
+		public int elsoNap { get; set; }
+		public int utolsoNap { get; set; }
+		public int mulasztottOrakSzama { get; set; }
 	}
-}
+
 
--- C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Program.cs
+++ C#/Feladatok/Hiányzások/Hianyzasok/Hianyzasok/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
+        List<Hianyzasok> hianyzasokLista = new List<Hianyzasok>();
+        StreamReader sr = new StreamReader("szeptember.csv");
+        string elsoSor = sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string sor = sr.ReadLine();
+            string[] sorSplit = sor.Split(";");
+            hianyzasokLista.Add(new Hianyzasok(sorSplit[0], sorSplit[1], Convert.ToInt32(sorSplit[2]), Convert.ToInt32(sorSplit[3]), Convert.ToInt32(sorSplit[4])));
+        }
+        int osszesMulasztott = 0;
+        foreach (var item in hianyzasokLista)
+        {
+            osszesMulasztott += item.mulasztottOrakSzama;
+        }
+        Console.WriteLine($"2. feladat\n\tÖsszes mulasztott órák száma: {osszesMulasztott} óra.");
+        Console.ReadKey();
+    }
+}
